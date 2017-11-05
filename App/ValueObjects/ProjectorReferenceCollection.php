d994734a87d36b50716227f29c8559a59cfce236
Barry O Sullivan <barry@tercet.io>
2017-11-05 22:25:47 +0000

    Wrote more tests to prove functionality

--- App/ValueObjects/ProjectorReferenceCollection.php
+++ App/ValueObjects/ProjectorReferenceCollection.php
@@ -5,18 +5,33 @@ use Illuminate\Support\Collection;
 // TODO: Write unit tests
 class ProjectorReferenceCollection extends Collection
 {
+    public function __construct($items = [])
+    {
+        parent::__construct($items);
+
+        $references = array_map(function(ProjectorReference $reference){
+            return $reference->toString();
+        }, $items);
+
+        $unique_references = array_unique($references);
+
+        if (count($unique_references) != count($references)) {
+            throw new \Exception("Duplicate projector references, not allowed");
+        }
+    }
+
     public function extract(string $mode): ProjectorReferenceCollection
     {
         return $this->filter(function(ProjectorReference $projector_reference) use ($mode) {
             return $projector_reference->mode == $mode;
-        });
+        })->values();
     }
 
     public function exclude(string $mode): ProjectorReferenceCollection
     {
         return $this->filter(function(ProjectorReference $projector_reference) use ($mode) {
             return $projector_reference->mode != $mode;
-        });
+        })->values();
     }
 
     public function extractNewProjectors(ProjectorPositionCollection $projector_positions)
---
+++ Tests/Unit/App/ValueObjects/ProjectorReferenceCollectionTest.php
@@ -0,0 +1,41 @@
+<?php namespace Tests\Unit\ValueObjects;
+
+use App\ValueObjects\ProjectorMode;
+use App\ValueObjects\ProjectorReference;
+use App\ValueObjects\ProjectorReferenceCollection;
+use Tests\Fakes\Projectors\RunFromStart;
+use Tests\Fakes\Projectors\RunOnce;
+
+class ProjectorReferenceCollectionTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_will_not_allow_duplicates()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("Duplicate projector references, not allowed");
+
+        $ref_1 = ProjectorReference::makeFromClass(RunFromStart::class);
+        new ProjectorReferenceCollection([$ref_1, $ref_1]);
+    }
+
+    public function test_excluding_by_mode()
+    {
+        $ref_1 = ProjectorReference::makeFromClass(RunFromStart::class);
+        $ref_2 = ProjectorReference::makeFromClass(RunOnce::class);
+        $collection = new ProjectorReferenceCollection([$ref_1, $ref_2]);
+
+        $excluded = $collection->exclude(ProjectorMode::RUN_ONCE);
+
+        $this->assertEquals([$ref_1], $excluded->all());
+    }
+
+    public function test_extracting_by_mode()
+    {
+        $ref_1 = ProjectorReference::makeFromClass(RunFromStart::class);
+        $ref_2 = ProjectorReference::makeFromClass(RunOnce::class);
+        $collection = new ProjectorReferenceCollection([$ref_1, $ref_2]);
+
+        $extracted = $collection->extract(ProjectorMode::RUN_ONCE);
+
+        $this->assertEquals([$ref_2], $extracted->all());
+    }
+}
