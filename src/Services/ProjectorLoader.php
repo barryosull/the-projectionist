0164af27b464bab89dcf1151d7d54cb4fb1a8eeb
Barry O Sullivan <barry@tercet.io>
2018-01-04 01:17:06 +0000

    Remove class

--- src/Services/ProjectorLoader.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Projectionist\Services;
-
-use Projectionist\ValueObjects\ProjectorReference;
-
-interface ProjectorLoader
-{
-    public function load(ProjectorReference $projectorId);
-}
