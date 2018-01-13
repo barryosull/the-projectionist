53494184091a83f96058527ec7dd05ee49679ea1
Barry O Sullivan <barry@tercet.io>
2018-01-13 23:40:50 +0000

    Continuining cleanup

--- src/Strategy/ProjectorPlayer.php
+++ src/Strategy/ProjectorPlayer.php
@@ -72,41 +72,35 @@ class ProjectorPlayer
 
     private function playProjectors(ProjectorPositionCollection $positions): ProjectorPositionCollection
     {
-        $group_by_position = $positions->groupByLastPosition();
+        $grouped_by_position = $positions->groupByLastPosition();
 
-        $group_by_position = $group_by_position->map(function($grouped_positions, $last_position){
+        $grouped_by_position = $grouped_by_position->map(function($grouped_positions, $last_position){
+            if ($this->thereWasAFailure()) {
+                return $grouped_positions;
+            }
             return $this->playProjectorsFromPosition($grouped_positions, $last_position);
         });
 
-        return new ProjectorPositionCollection($group_by_position->flatten()->toArray());
+        return new ProjectorPositionCollection($grouped_by_position->flatten()->toArray());
     }
 
     private function playProjectorsFromPosition(ProjectorPositionCollection $positions, $last_position): ProjectorPositionCollection
     {
-        if ($this->thereWasAFailure()) {
-            return $positions;
-        }
-
         $event_stream = $this->event_store->getStream($last_position);
 
         while ($event = $event_stream->next()) {
             if ($event == null) {
                 break;
             }
-            $positions = $this->playEventIntoProjectors($event, $positions);
+            $positions = $positions->map(function(ProjectorPosition $position) use ($event) {
+                return $this->playEventIntoProjector($event, $position);
+            });
 
             if ($this->thereWasAFailure()) {
                 return $positions;
             }
         }
         return $positions;
-    }
-
-    private function playEventIntoProjectors(EventWrapper $event, ProjectorPositionCollection $positions)
-    {
-        return $positions->map(function(ProjectorPosition $position) use ($event) {
-           return $this->playEventIntoProjector($event, $position);
-        });
     }
 
     private function playEventIntoProjector(EventWrapper $event, ProjectorPosition $projector_position): ProjectorPosition
